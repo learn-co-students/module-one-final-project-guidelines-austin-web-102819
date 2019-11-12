d5f50fd5a283e32b0abfa934d8f064af9481e939
fmwong77 <fongmeiw@gmail.com>
2019-11-12 12:07:12 -0600

    created tables in epicure.sqlite and defined relationships

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 require 'bundler'
 Bundler.require
 
+require "sinatra/activerecord"
+require 'rake'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/epicure.sqlite')
 require_all 'app'
