8e43bcbe46e8dc08b65c1a84569f3b6403722547
fmwong77 <fongmeiw@gmail.com>
2019-11-12 14:08:35 -0600

    fixing git merge

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,5 @@
 require 'bundler'
 Bundler.require
 
-require "sinatra/activerecord"
-require 'rake'
-
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: '../db/epicure.sqlite')
-# ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/epicure.sqlite')
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-# require_all 'app'
