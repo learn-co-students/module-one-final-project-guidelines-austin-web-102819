a1901028774a49a6557dafea4ba5e98c9f67b33e
fmwong77 <fongmeiw@gmail.com>
2019-11-12 13:32:52 -0600

    test db connection with user_login method in run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,31 @@
 require_relative '../config/environment'
+require 'pry'
 
+def run
+    puts "Key in your first name"
+    user_input = get_user_input()
+    
+    if user_input == "exit"
+        puts "Goodbye!"
+    else
+        # verify valid user
+        user_login(user_input)
+    end
+end
 
+def get_user_input
+    gets.chomp
+end
 
-puts "HELLO WORLD"
+def user_login(logged_in_user)
+    user = User.get_user(logged_in_user)
+    # binding.pry
+    if user
+        puts "Hello #{john.first_name}"
+    else
+        puts "User does not exist."
+    end
+end
+
+run
+
--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,7 @@ Bundler.require
 require "sinatra/activerecord"
 require 'rake'
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/epicure.sqlite')
-require_all 'app'
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: '../db/epicure.sqlite')
+# ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/epicure.sqlite')
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+# require_all 'app'
