aa4db32a725af8ad5d152dc4c6223140763333f0
rachfuerst <rach.fuerst@gmail.com>
2019-11-12 16:29:57 -0600

    aadded search_word method

--- app/dictionary_cli.rb
+++ app/dictionary_cli.rb
@@ -8,10 +8,20 @@ puts "Welcome to the WordBank App!"
 def user_name
     puts "Please create a username to create your account:"
     name= gets.chomp
-    puts "Your username is: #{name}"
+    puts "Your ussername is: #{name}"
     User.create(name: name)
+    puts "Thanks for opening an account with WordBank!"
     name
 end
+
+def search_word
+    puts "You can now search for any word. Enter your word below and hit 'Enter'"
+    word = gets.chomp
+    result= Word.find_by(:word => word)
+    puts "*****************************"
+    puts "Definition of #{word}: #{result.definition}"
+end
+
 
 
 
