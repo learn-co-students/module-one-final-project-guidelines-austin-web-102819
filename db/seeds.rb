27e404a3a188a8724b817ed4647a0d185fd4cc6d
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-11-12 14:13:10 -0600

    added seed data

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+workbag = Word.create(word: "workbag", definition: "a bag for holding implements and materials for work, especially needlework.", part_of_speech: "noun")
+
+curmudgeon = Word.create(word: "curmudgeon", definition: "a bad-tempered, difficult, cantankerous person." , part_of_speech:"noun")
+ 
+irascible = Word.create(word: "irascible", definition: "easily provoked to anger; very irritable." , part_of_speech:"adverb")
+ 
+goof = Word.create(word: "goof", definition: "to blunder; make an error, misjudgment." , part_of_speech:"verb")
+
+impeach = Word.create(word: "impeach", definition: "to accuse (a public official) before an appropriate tribunal of misconduct in office." , part_of_speech:"verb")
+
+witch = Word.create(word: "witch", definition: "a person, now especially a woman, who professes or is supposed to practice magic or sorcery; a sorceress." , part_of_speech:"noun")
+
+clueless = Word.create(word: "clueless", definition: "ignorant, unaware, or uninformed." , part_of_speech:"adjective")
+
+hat = Word.create(word: "hat", definition: "a shaped covering for the head, usually with a crown and brim, especially for wear outdoors." , part_of_speech:"noun")
+
+downtown = Word.create(word: "downtown", definition: "the main business section of a city." , part_of_speech:"noun") 
+
+shit = Word.create(word: "shit", definition: "excrement; feces." , part_of_speech:"noun")
