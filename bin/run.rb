1d28b2cc16e9510032879f40bddccbeec0662c86
rachfuerst <rach.fuerst@gmail.com>
2019-11-13 17:55:00 -0600

    api/beginning of control flow

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
-require_relative '../config/environment'
+require_relative '../config/environment.rb'
+
+welcome
+user_object= user_name
+word= search_prompt
+api_url=build_url(word)
+word_array=make_request(api_url)
+get_definition(user_object, word_array)
+search_again
 
 
-
-puts "HELLO WORLD"
