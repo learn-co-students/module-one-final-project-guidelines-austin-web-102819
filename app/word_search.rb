2237aeb3e098305f36c75686bce1b4d38105c2e5
rachfuerst <rach.fuerst@gmail.com>
2019-11-13 12:17:10 -0600

    successful request to the wordapi

--- app/word_search.rb
+++ app/word_search.rb
@@ -2,5 +2,35 @@ require 'rest-client'
 require 'uri'
 require 'net/http'
 require 'openssl'
+require 'pry'
+require 'json'
+# require 'uri'
+# require 'net/http'
+# require 'openssl'
+# "https://wordsapiv1.p.rapidapi.com/words/curmudgeon"
 
-WORDS_API_URL= "https://wordsapiv1.p.mashape.com/words/"
+WORDS_API_URL= "https://wordsapiv1.p.rapidapi.com/words/"
+
+def build_url(word_object)
+    url = "#{WORDS_API_URL}#{word_object}"
+    url
+end
+
+def make_request(builtUrl)
+    url = URI(builtUrl)
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'wordsapiv1.p.rapidapi.com'
+    request["x-rapidapi-key"] = '2df3014c24mshd2b068316cb08b8p13a365jsn553cbf262f9d'
+
+    response = http.request(request)
+    parsed_response = JSON(response.read_body)
+    binding.pry
+    puts response.read_body
+end
+
+make_request(build_url("hat"))
