688d6e669bcfcd46c1c998ff43548feda4149eb2
rachfuerst <rach.fuerst@gmail.com>
2019-11-12 21:02:38 -0600

    testing save_word method_UNSUCCESSFUL

--- app/dictionary_cli.rb
+++ app/dictionary_cli.rb
@@ -1,12 +1,10 @@
-# require_relative './models/user.rb'
-
 require 'pry'
 # Output welcome message
 puts "Welcome to the WordBank App!"
 puts "==========================="
 #Username creation
 
-def user_name
+def user_name #This is working on the User model
     puts "Please create a username to create your account:"
     puts "==================================="
     name= gets.chomp
@@ -17,7 +15,7 @@ def user_name
     name
 end
 
-def search_word
+def search_word #this is working on the Word model
     puts "You can now search for any word. Enter your word below and hit 'Enter'"
     word = gets.chomp
     result= Word.find_by(:word => word) 
@@ -26,9 +24,10 @@ def search_word
     result
 end
 
-def save_word
+def save_word #this is working on BOTH models. We need to chain the methods by passing them as arguments here. 
     puts "Would you like to save this to your WordBank? (y/n)"
     answer= gets.chomp
+    binding.pry
     if answer == "y"
         WordList.create(user_id: user , word_id: word) #need to find a way to have the results from #search_word and #user_name input into the DB
         # binding.pry
@@ -39,8 +38,6 @@ def save_word
     end
 end
 
-
-# Save the Username
-# def save_name(user_name)
-#     User.create(name: user_name)
-# end
+# This works when we put all the work inside the user_name method.
+# This is because the save_word method cannot access the variables in user_name and search_word
+# Next steps: Chain the methods together by passing in the first two methods as arguments: save_word(user_name, search_word)
