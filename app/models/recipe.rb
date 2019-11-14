f0e6e84eb22747e8d26c56dfb81f15cea16402ec
fmwong77 <fongmeiw@gmail.com>
2019-11-13 20:50:47 -0600

    merge code

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,15 +1,9 @@
 class Recipe < ActiveRecord::Base
     has_many :meals
     has_many :users, through: :meals
-<<<<<<< HEAD
 
     def delete_recipe_by_meal
         # Recipe.meals.all.find_by[]
 
     end
-end 
-=======
->>>>>>> origin/master
-
-
 end 
